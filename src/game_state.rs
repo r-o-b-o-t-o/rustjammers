40e623c81d3762e011fdd47453fc5870be657e6f
Axel Cocat <ax.cocat@gmail.com>
2018-10-15 16:33:36 +0200

    Game state init & dispose

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,6 +1,8 @@
 use player::Player;
 use frisbee::Frisbee;
 
+use std::mem::transmute;
+
 pub struct SharedData {
     pub p1_x:       f64,
     pub p1_y:       f64,
@@ -12,11 +14,30 @@ pub struct SharedData {
 
     pub zbee_x:     f64,
     pub zbee_y:     f64,
-
 }
 
 pub struct GameState {
     pub p1:      Player,
     pub p2:      Player,
     pub frisbee: Frisbee,
+}
+
+impl GameState {
+    #[no_mangle]
+    pub extern fn initialize() -> *mut Self {
+        unsafe { transmute(Box::new(Self::new())) }
+    }
+
+    #[no_mangle]
+    pub extern fn dispose(ptr: *mut Self) {
+        let _state: Box<Self> = unsafe { transmute(ptr) };
+    }
+
+    fn new() -> Self {
+        Self {
+            p1:      Player::new(),
+            p2:      Player::new(),
+            frisbee: Frisbee::new()
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,3 @@ mod player;
 mod frisbee;
 mod vector2;
 mod game_state;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn multiply_vector() {
-        use vector2::Vector2;
-        let q: Vector2 = Vector2::new(4.0, 8.0);
-        let mut v: Vector2 = Vector2::new(2.0, 4.0);
-        v *= 2.0;
-        assert_eq!(q, v);
-    }
-}
