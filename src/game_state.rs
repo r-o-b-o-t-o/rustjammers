425de800556c02771cc3805190bc4b901e4f1d82
Jerrain Yohann <kryod@users.noreply.github.com>
2018-10-15 15:52:13 +0200

    struct shared data

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,8 +1,22 @@
 use player::Player;
 use frisbee::Frisbee;
 
+pub struct SharedData {
+    pub p1_x:       f64,
+    pub p1_y:       f64,
+    pub p1_score:   f64,
+
+    pub p2_x:       f64,
+    pub p2_y:       f64,
+    pub p2_score:   f64,
+
+    pub zbee_x:     f64,
+    pub zbee_y:     f64,
+
+}
+
 pub struct GameState {
     pub p1:      Player,
-    pub pb2:     Player,
+    pub p2:      Player,
     pub frisbee: Frisbee,
 }
--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,6 @@
 use vector2::Vector2;
 
 pub struct Player {
-    pub pos: Vector2
+    pub pos: Vector2,
+    pub score: i8
 }
